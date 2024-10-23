c1c0cf3c9aa85485235c3139bf6a8a313576d069
Ryusei-Baba <babaryusei.kw@gmail.com>
2024-10-23 22:02:13 +0900

    update

--- login.sh
+++ login.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+eval "xhost +local:"
+eval "docker start box3_humble"
 
-eval "docker container exec -it box3_humble env TERM=xterm-256color /bin/bash"
+eval "docker container exec -it --env="DISPLAY" box3_humble env TERM=xterm-256color /bin/bash"
